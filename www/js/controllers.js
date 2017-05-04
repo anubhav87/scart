26cc77f0fd43d3bc816744886f7b8f4b9d87d6ac
anubhav <ani.anubhav@gmail.com>
2017-05-04 12:50:37 +0530

    Added slick controls on dashboard page

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -25,7 +25,207 @@ angular.module('app.controllers', [])
     }
 })
 
-.controller('dashboardCtrl', function($scope,$rootScope,$ionicHistory,sharedUtils,$state,$ionicSideMenuDelegate) {
+.controller('dashboardCtrl', function($scope,$rootScope,$ionicHistory,sharedUtils,$state,$ionicSideMenuDelegate, $timeout) {
+
+
+
+    //====================================
+    // Slick Banner
+    //====================================
+    $scope.banner_images = [
+      {src: "http://localhost:8082/ionic_apps/scart/php_code/e_comm/big-1.jpg", title: 1}, 
+      {src: "http://localhost:8082/ionic_apps/scart/php_code/e_comm/big-2.jpg", title: 2}, 
+      {src: "http://localhost:8082/ionic_apps/scart/php_code/e_comm/big-3.jpg", title: 3}, 
+      {src: "http://localhost:8082/ionic_apps/scart/php_code/e_comm/big-4.jpg", title: 4}, 
+      {src: "http://localhost:8082/ionic_apps/scart/php_code/e_comm/big-1.jpg", title: 5}, 
+      {src: "http://localhost:8082/ionic_apps/scart/php_code/e_comm/big-2.jpg", title: 6}, 
+      {src: "http://localhost:8082/ionic_apps/scart/php_code/e_comm/big-3.jpg", title: 7}, 
+      {src: "http://localhost:8082/ionic_apps/scart/php_code/e_comm/big-4.jpg", title: 8}, 
+    ];
+
+    $scope.slickConfig_img_Loaded = true;
+
+    $scope.slickCurrentIndex = 0;
+    $scope.slickConfig_img = {
+      dots: true,
+      autoplay: false,
+      initialSlide: 0,
+      infinite: true,
+      autoplaySpeed: 5000,
+      centerMode: true,
+      method: {},
+    };
+
+
+    //====================================
+    // Slick Slider
+    //====================================
+    $scope.slider_images = [
+      {src: "http://localhost:8082/ionic_apps/scart/php_code/e_comm/small-1.jpg", title: 1}, 
+      {src: "http://localhost:8082/ionic_apps/scart/php_code/e_comm/small-2.jpg", title: 2}, 
+      {src: "http://localhost:8082/ionic_apps/scart/php_code/e_comm/small-3.jpg", title: 3}, 
+      {src: "http://localhost:8082/ionic_apps/scart/php_code/e_comm/small-4.jpg", title: 4}, 
+      {src: "http://localhost:8082/ionic_apps/scart/php_code/e_comm/small-5.jpg", title: 5}, 
+      {src: "http://localhost:8082/ionic_apps/scart/php_code/e_comm/small-6.jpg", title: 6}, 
+      {src: "http://localhost:8082/ionic_apps/scart/php_code/e_comm/small-1.jpg", title: 7}, 
+      {src: "http://localhost:8082/ionic_apps/scart/php_code/e_comm/small-2.jpg", title: 8}, 
+    ];
+
+    $scope.slickConfig_slider_Loaded = true;
+
+    $scope.slickConfig_slider = {
+      dots: false,
+      autoplay: false,
+      infinite: true,
+      autoplaySpeed: 1000,
+      slidesToShow: 3,
+      slidesToScroll: 3,
+      arrows: false,
+      method: {}
+    };
+
+
+    //====================================
+    // Slick 1
+    //====================================
+    $scope.number1 = [1, 2, 3, 4, 5, 6, 7, 8];
+    $scope.slickConfig1Loaded = true;
+    $scope.updateNumber1 = function () {
+      $scope.slickConfig1Loaded = false;
+      $scope.number1[2] = '123';
+      $scope.number1.push(Math.floor((Math.random() * 10) + 100));
+      $timeout(function () {
+        $scope.slickConfig1Loaded = true;
+      }, 5);
+    };
+    $scope.slickCurrentIndex = 0;
+    $scope.slickConfig = {
+      dots: true,
+      autoplay: true,
+      initialSlide: 0,
+      infinite: true,
+      autoplaySpeed: 5000,
+      method: {},
+      event: {
+        beforeChange: function (event, slick, currentSlide, nextSlide) {
+          console.log('before change', Math.floor((Math.random() * 10) + 100));
+        },
+        afterChange: function (event, slick, currentSlide, nextSlide) {
+          $scope.slickCurrentIndex = currentSlide;
+        },
+        breakpoint: function (event, slick, breakpoint) {
+          console.log('breakpoint');
+        },
+        destroy: function (event, slick) {
+          console.log('destroy');
+        },
+        edge: function (event, slick, direction) {
+          console.log('edge');
+        },
+        reInit: function (event, slick) {
+          console.log('re-init');
+        },
+        init: function (event, slick) {
+          console.log('init');
+        },
+        setPosition: function (evnet, slick) {
+          console.log('setPosition');
+        },
+        swipe: function (event, slick, direction) {
+          console.log('swipe');
+        }
+      }
+    };
+
+    //====================================
+    // Slick 2
+    //====================================
+    $scope.number2 = [{label: 1, otherLabel: 1}, {label: 2, otherLabel: 2}, {label: 3, otherLabel: 3}, {
+      label: 4,
+      otherLabel: 4
+    }, {label: 5, otherLabel: 5}, {label: 6, otherLabel: 6}, {label: 7, otherLabel: 7}, {label: 8, otherLabel: 8}];
+    $scope.slickConfig2Loaded = true;
+    $scope.updateNumber2 = function () {
+      $scope.slickConfig2Loaded = false;
+      $scope.number2[2] = 'ggg';
+      $scope.number2.push(Math.floor((Math.random() * 10) + 100));
+      $timeout(function () {
+        $scope.slickConfig2Loaded = true;
+      });
+    };
+
+    $scope.slickConfig2 = {
+      autoplay: true,
+      infinite: true,
+      autoplaySpeed: 1000,
+      slidesToShow: 3,
+      slidesToScroll: 3,
+      method: {}
+    };
+
+
+    //====================================
+    // Slick 3
+    //====================================
+    $scope.number3 = [{label: 1}, {label: 2}, {label: 3}, {label: 4}, {label: 5}, {label: 6}, {label: 7}, {label: 8}];
+    $scope.slickConfig3Loaded = true;
+    $scope.slickConfig3 = {
+      method: {},
+      dots: true,
+      infinite: false,
+      speed: 300,
+      slidesToShow: 4,
+      slidesToScroll: 4,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+    }; 
+    
+
+    //====================================
+    // Slick 4
+    //====================================
+    $scope.number4 = [{label: 225}, {label: 125}, {label: 200}, {label: 175}, {label: 150}, {label: 180}, {label: 300}, {label: 400}];
+    $scope.slickConfig4Loaded = true;
+    $scope.updateNumber4 = function () {
+      $scope.slickConfig4Loaded = false;
+      $scope.number4[2].label = 123;
+      $scope.number4.push({label: Math.floor((Math.random() * 10) + 100)});
+      $timeout(function () {
+        $scope.slickConfig4Loaded = true;
+      });
+    };
+    $scope.slickConfig4 = {
+      method: {},
+      dots: true,
+      infinite: true,
+      speed: 300,
+      slidesToShow: 1,
+      centerMode: true,
+      variableWidth: true
+    };
 
 })
 
